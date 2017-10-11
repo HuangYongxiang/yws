053d6182111143a45a7900e8138c2a165b0312f0
HuangYongxiang <15838309259@139.com>
2017-10-11 13:05:23 +0800

    11111

---
+++ app/src/main/java/com/example/wangchuang/yws/activity/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.wangchuang.yws.activity;
+
+import com.example.wangchuang.yws.R;
+import com.example.wangchuang.yws.base.BaseActivity;
+import com.example.wangchuang.yws.utils.eventbus.EventCenter;
+import com.example.wangchuang.yws.utils.netstatus.NetUtils;
+
+public class LoginActivity extends BaseActivity {
+
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initPresenter() {
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    protected boolean isBindEventBusHere() {
+        return false;
+    }
+
+    @Override
+    protected void onEventComming(EventCenter eventCenter) {
+    }
+
+    @Override
+    protected void onNetworkConnected(NetUtils.NetType type) {
+
+    }
+
+    @Override
+    protected void onNetworkDisConnected() {
+    }
+}
---
+++ app/src/main/java/com/example/wangchuang/yws/content/Constants.java
@@ -0,0 +1,11 @@
+package com.example.wangchuang.yws.content;
+
+/**
+ * Created by Administrator on 2017/10/11.
+ */
+
+public class Constants {
+    public static final String BaseUrl="47.95.118.150/index.php/App/Members/";
+    public static final String loginUrl="member_login";//登录
+    public static final String registerUrl="members_register";//注册
+}
