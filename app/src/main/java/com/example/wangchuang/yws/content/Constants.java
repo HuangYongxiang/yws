1c90cc43383d58d0dc77228ef6b7210edcfc7857
DESKTOP-FF1OMPO\zhaoming <88395593@qq.com>
2017-10-11 16:43:34 +0800

    base

--- app/src/main/java/com/example/wangchuang/yws/content/Constants.java
+++ app/src/main/java/com/example/wangchuang/yws/content/Constants.java
@@ -16,6 +16,10 @@ public class Constants {
 
     public static final String loginUrl="member_login";//登录
     public static final String registerUrl="members_register";//注册
+    public static final String memberUrl1="send_sms";//验证码
+    public static final String memberUrl2="back_sms";//找回密码验证码
+    public static final String refactUrl="back_password";//修改密码
+
     public static final String mainListUrl="index";//首页
 
 
