1194f840474f69237c0928decec433dfec69553a
HuangYongxiang <15838309259@139.com>
2017-10-11 16:34:02 +0800

    11111

--- app/src/main/java/com/example/wangchuang/yws/content/Constants.java
+++ app/src/main/java/com/example/wangchuang/yws/content/Constants.java
@@ -5,7 +5,10 @@ package com.example.wangchuang.yws.content;
  */
 
 public class Constants {
-    public static final String BaseUrl="47.95.118.150/index.php/App/Members/";
+    public static final String BaseUrl="http://47.95.118.150/index.php/App/Members/";
     public static final String loginUrl="member_login";//登录
     public static final String registerUrl="members_register";//注册
+    public static final String memberUrl1="send_sms";//验证码
+    public static final String memberUrl2="back_sms";//找回密码验证码
+    public static final String refactUrl="back_password";//修改密码
 }
