88f8e1c87c4dd923b2d881d56fc5e56e29a7d823
HuangYongxiang <15838309259@139.com>
2017-10-17 22:43:01 +0800

    备份   wuyong2

--- app/src/main/java/com/example/wangchuang/yws/content/Constants.java
+++ app/src/main/java/com/example/wangchuang/yws/content/Constants.java
@@ -14,15 +14,15 @@ public class Constants {
     public static final int IMAGE_PICKER = 1;
 
 
-    public static final String BaseUrl="http://47.95.118.150/index.php/App/Members/";
+    public static final String BaseUrl="http://47.95.118.150/index.php/App/";
     public static final String RequestUrl="http://wc306.com/index.php/App/";
 
 
-    public static final String loginUrl="member_login";//登录
-    public static final String registerUrl="members_register";//注册
-    public static final String memberUrl1="send_sms";//验证码
-    public static final String memberUrl2="back_sms";//找回密码验证码
-    public static final String refactUrl="back_password";//修改密码
+    public static final String loginUrl="Members/member_login";//登录
+    public static final String registerUrl="Members/members_register";//注册
+    public static final String memberUrl1="Members/send_sms";//验证码
+    public static final String memberUrl2="Members/back_sms";//找回密码验证码
+    public static final String refactUrl="Members/back_password";//修改密码
 
     public static final String mainListUrl="Index/index";//首页
     public static final String mainDetailUrl="UserDetails/article_details";//首页
